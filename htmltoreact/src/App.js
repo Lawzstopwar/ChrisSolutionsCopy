98df388cec608f5f8d96d379f9ebc777dbdce854
Lawzstopwar <lawzstopwar@gmail.com>
2023-06-07 15:48:23 +0100

    create react app added

---
+++ htmltoreact/src/App.js
@@ -0,0 +1,55 @@
+
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App"><body> 
+    <div class="container min-h-screen bg-[url('images/background.png')] bg-center bg-cover px-28 py-5 relative">
+        <nav class="flex items-center">
+        
+            <img src="images/specs10.2.png" class="w-full sm:w-1/4 sm:absolute -top- left-0"></img>
+            <img src="" class="w-40 cursor-pointer"></img>
+    
+            
+            <ul class="flex-1 text-center">
+                <li class="list-none inline-block px-5"><a href="#"  class="no-underline text-red-800 px-2">Login</a></li>
+                <li class="list-none inline-block px-5"><a href="#"  class="no-underline text-yellow-400 px-2">Home</a></li>
+                <li class="list-none inline-block px-5"><a href="#"  class="no-underline text-white px-2">About</a></li>
+                <li class="list-none inline-block px-5"><a href="#"  class="no-underline text-orange-500 px-2">Features</a></li>
+                <li class="list-none inline-block px-5"><a href="#"  class="no-underline text-blue-400 px-2">Contact</a></li>
+                
+            </ul>
+            
+            <img src="images/analysis.1.jpg"class="w-30 cursor-pointer">
+
+            </img>
+
+        </nav>
+        <div class="text-white mt-40 max-w-xl">
+            <h1 class="text-3xl font-semibold leading-normal text-blue-400">Objective<br>To achieve high levels of accuracy in delivery and performance</br>
+            <span class="font-light"></span></h1>
+            <p>Aim to Utilize customer specifications and requirements in acquiring precise desired outcomes </p>
+            
+        <div class="mt-10">
+            <a href="#" class="bg-yellow-300 rounded-3xl py-3 px-8 font-medium inline-block mr-4 hover:bg-transparent hover:border-yellow-300 hover:text-white duration-300 hover:border border-transparent">Make enquiry</a>
+            <a href="#"class="bg-yellow-300 rounded-3xl py-3 px-8 font-medium inline-block mr-4 hover:bg-transparent hover:border-yellow-300 hover:text-white duration-300 hover:border border-transparent" >Download Resume <span class="text-lg inline-block rotate-90 ">&#10148</span></a>
+        </div>
+        
+        <img src="#" class="w-full xl:w-1/2 xl:absolute -bottom-0 right-2 "></img>
+       
+        <footer>Copyright &copy; 2023</footer>
+        </div>
+        </div>
+        </body>
+        </div>
+  )
+  
+}
+
+
+    
+
+  
+  
+export default App;
